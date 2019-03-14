2e9d8b32623c5bc9f7f9d474cbedd95a302f0d66
victornieto <vnieto@ucsc.edu>
2019-03-14 00:28:38 -0700

    Fixed tie breakers for one pair; two pair. Fixed issues with calculating total point

---
+++ PokerGame-updated/mcts_implement.py
@@ -0,0 +1,25 @@
+from mcts_node import MCTSNode
+from random import choice
+from math import sqrt, log
+
+num_nodes = 1000
+explore_factor = 2
+
+def traverse_nodes(node, game, state, identity):
+    #return node
+    pass
+
+def expand_leaf(node, game, state):
+    #return new_leaf
+    pass
+
+def rollout(game, state):
+    #return game_points
+    pass
+
+def backpropagate(node, won):
+    pass
+
+def think(game, state):
+    #return string
+    pass
